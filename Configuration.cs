e6a16c2841a68c16b20ea19c8c360a01567d7753
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-02-21 22:21:09 -0300

    Ajuste Program.cs

--- Configuration.cs
+++ Configuration.cs
@@ -5,6 +5,15 @@ namespace Blog
         // JWT - Token - Json Web Token
         public static string JwtKey = "2314f4041a594f1bb6d9656d53d2ff4e";
         public static string ApiKeyName = "api_key";
-        public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";    
+        public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
+        public static SmtpConfiguration Smtp = new SmtpConfiguration(); // Intanciando classe interna
+
+        public class SmtpConfiguration
+        {
+            public string Host { get; set; }
+            public int Port { get; set; } = 25;     
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }    
     }
 }
