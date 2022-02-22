5c5b16bd599d4f6a1867b0d124fc4794bc95ce7a
Samuel-Lucas <samuel.moreira10@outlook.com>
2022-02-21 21:58:23 -0300

    Autenticação ApiKey

--- Configuration.cs
+++ Configuration.cs
@@ -3,6 +3,8 @@ namespace Blog
     public static class Configuration
     {
         // JWT - Token - Json Web Token
-        public static string JwtKey { get; set; } = "2314f4041a594f1bb6d9656d53d2ff4e";        
+        public static string JwtKey = "2314f4041a594f1bb6d9656d53d2ff4e";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";    
     }
 }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,11 +1,18 @@
+using Blog.Attributes;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
 {
     [ApiController]
-    [Route("/")]
+    [Route("")]
     public class HomeController : ControllerBase
     {
+        [HttpGet("")]
+        //[ApiKey]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
         
     }
 }
